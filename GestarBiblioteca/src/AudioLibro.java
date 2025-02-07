34620103ce8be40dbfd32e57b1838256d7296942
DaniGarcia-hub <dangarqui2@alu.edu.gva.es>
2025-02-07 20:18:49 +0100

    Delete GestarBiblioteca directory

--- GestarBiblioteca/src/AudioLibro.java
+++
@@ -1,8 +0,0 @@
-package GestarBiblioteca.src;
-
-public class AudioLibro extends Libro {
-    public AudioLibro(String codLibro, String titulo, String idioma, String editorial, String autor) {
-        super(codLibro, titulo, idioma, editorial, autor);
-        registrarAudioLibro();
-    }
-}
--- GestarBiblioteca/src/Fisico.java
+++
@@ -1,7 +0,0 @@
-package GestarBiblioteca.src;
-
-public class Fisico extends Libro{
-    public Fisico(String codLibro, String titulo, String idioma, String editorial, String autor) {
-        super(codLibro, titulo, idioma, editorial, autor);
-    }
-}
